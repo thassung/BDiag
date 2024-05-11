e6dd7eac2ad33bc1854a4556af53b2d44536efa2
thassung <105700459+thassung@users.noreply.github.com>
2024-05-11 19:35:38 +0700

    Initial commit

---
+++ playground/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+# class Film(models.Model):
+# 	title	= models.CharField(max_length=200)
+# 	year	= models.IntegerField()
+# 	votes	= models.IntegerField()
+# 	rating	= models.FloatField()
+# 	rank	= models.IntegerField()
+
+# class FilmForm(ModelForm):
+#     class Meta:
+#         model = Film
+#         fields = [
+#             'title', 'year', 'votes', 'rating', 'rank'
+#         ]
+
